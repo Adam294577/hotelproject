3387887529eb2cb078f4ac23a6e352712ed8a2b9
Satoru <adam29457756@gmail.com>
2025-01-18 10:31:10 +0800

    更新domain

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     imgurClientId: process.env.IMGUR_CLIENT_ID,
     public: {
       backendOptions: {
-        baseURL: "https://hotelproject-backend-nnmd.onrender.com/api/v1",
+        baseURL: "https://hotelproject-backend-2.onrender.com/api/v1",
         headers: {
           Authorization: "",
         },
